23332fca4fabb7a11fcbd2fb05698bd739499baf
Agnibho1678 <73741624+Agnibho1678@users.noreply.github.com>
2020-11-24 18:21:45 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -78,28 +78,6 @@ function draw() {
   
   thread.display();
 
-  function isTouching(object1,object2)
-{
-  if(object1.x-object2.x<object2.width/2+object1.width/2 
-    && object1.y-object2.y<object2.height/2+object1.height/2
-    && object2.x-object1.x<object2.width/2+object1.width/2
-    && object2.y-object1.y<object2.height/2+object1.height/2 )
-  {
- 
-
-    return true;
-  }
-  else 
-  {
-
-    return false;
-  }
-
-
-
-}
-
-
 function detectCollision(body1,body2)
 {
   body1pos=body1.body.position;
